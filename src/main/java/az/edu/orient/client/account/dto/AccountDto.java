3b1c0cd79e90402354fb9e077f2767636d47b002
Murad Imanbayli <muradimanbayli@gmail.com>
2024-07-24 19:07:31 +0400

    first commit

---
+++ src/main/java/az/edu/orient/client/account/dto/AccountDto.java
@@ -0,0 +1,20 @@
+package az.edu.orient.client.account.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDto {
+    private String name;
+    private Currency currency;
+    private BigDecimal balance;
+    private long userId;
+}
